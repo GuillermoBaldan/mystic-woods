4e2bfa08b70d71256672960110f7576167cf0634
GuillermoBaldan <guillermobaldan@gmail.com>
2024-01-04 21:12:22 +0100

    controls modified

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,20 +3,20 @@ extends CharacterBody2D
 const speed = 100
 
 func _physics_process(delta):
-	pass
+	player_movement(delta)
 
 func player_movement(delta):
 	
-	if Input.is_action_just_pressed("ui_right"):
+	if Input.is_action_pressed("ui_right"):
 		velocity.x = speed
 		velocity.y = 0
-	elif Input.is_action_just_pressed("ui_left"):
+	elif Input.is_action_pressed("ui_left"):
 		velocity.x = -speed
 		velocity.y = 0
-	elif Input.is_action_just_pressed("ui_down"):
+	elif Input.is_action_pressed("ui_down"):
 		velocity.y = speed
 		velocity.x = 0
-	elif Input.is_action_just_pressed("ui_up"):
+	elif Input.is_action_pressed("ui_up"):
 		velocity.y = -speed
 		velocity.x = 0
 	else:
